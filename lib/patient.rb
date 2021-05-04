e955960d50439f15d84a6ed3a7b67c53d0e4ba22
Adam Schwartz <adamberard24@gmail.com>
2021-05-03 20:17:02 -0400

    wasn't passing for some reason

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+
+    @@all = []
+
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def doctors
+        appointments.map do |appointment|
+            appointment.doctor
+        end
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+    end
+end
 
